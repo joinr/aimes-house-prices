ecd52c24e9e0d6943a2f37571e39e91ef3673226
tom <tom@that-place>
2019-01-29 06:55:36 -0600

    minor change, almost done

--- src/clj_ml_wkg/tablesaw.clj
+++ src/clj_ml_wkg/tablesaw.clj
@@ -22,7 +22,9 @@
   (column-name [c])
   (column-type [c])
   ;;maybe change to column-size?
-  (entry-count [c] "This is lame, wish we had ICounted..."))
+  (entry-count [c] "This is lame, wish we had ICounted...")
+  (get-entry [c idx])
+  (set-entry [c idx v]))
 
 ;;more to follow...
 (defprotocol IDestructiveColumnOps
@@ -73,41 +75,38 @@
 (defn column->unique-count [^Column col]
   (.uniqueCount col))
 
-
-(defn map-column [col f]
-  (parallel/parallel-for idx (entry-count double-col)
-    (.set double-col idx
-          (double (double-fn (.getDouble double-col idx))))))
-
-(def +types+ {"DOUBLE"  "Double"
-              "LONG"    "Long"
-              "SHORT"   "Short"
-              "INTEGER" ["Integer" "IntColumn"]
-              "FLOAT"   "Float"
-              "BOOLEAN" "Boolean"
-              "STRING"  "String"})
-
-(def cols (for [t types]
-            (let [[t ]
-                  upped (str (clojure.string/upper-case (subs t 0 1))
-                             (clojure.string/lower-case (subs 1 t)))]
-              {:getter (symbol (str "get" upped "."))
-               :type   (symbol (str upped "Column"))})))
-
-(defmacro for-column [col type f]
-  (let [getter (symbol (get getters type "getObject."))]
-    (parallel/parallel-for idx (entry-count double-col)
-    (.set double-col idx
-          (double (double-fn (.getDouble double-col idx)))))))
+(def coltypes 
+  '{"DOUBLE"  [DoubleColumn double]
+    "LONG"    [LongColumn long]  
+    "SHORT"   [ShortColumn short]
+    "INTEGER" [IntColumn int]
+    "FLOAT"   [FloatColumn float]
+    "BOOLEAN" [BooleanColumn boolean]
+    "STRING"  [StringColumn str]})
+   
+(defmacro typed-apply [type col f]
+  (let [[coltype coercion] (or (coltypes type)
+                               (throw (ex-info "Uknown Column Type"
+                                               {:column-type type}))) 
+        typed-col (with-meta (gensym "typed-col") {:type  type})]
+    `(let [~typed-col ~col
+           func#      ~f]
+       (parallel/parallel-for idx# (entry-count ~typed-col)
+                              (->> (.get ~typed-col idx#)
+                                   ~f
+                                   ~coercion
+                                   (.set ~typed-col idx#))))))
 
 ;;wrapper to help us out here...could use macrology to
 ;;make this simpler....are there a set of base operations
 ;;we can use to define appropriate wrappers?
 (deftype tablesaw-column [^Column col]
   IColumn
-  (column-name [c]     (.name col))
-  (column-type [c]     (.type col))
-  (entry-count [c]     (.count col))
+  (column-name [c]       (.name col))
+  (column-type [c]       (.type col))
+  (entry-count [c]       (.count col))
+  (get-entry   [c idx]   (.get col (int idx))) 
+  (set-entry   [c idx v] (.set col (int idx) v))
   IColumnEntries  ;;maybe revisit? could also cache.
   (entries [c] (vec (.asList col)))
   IUnique
@@ -116,17 +115,19 @@
   IMissing
   (-missing-count [c] (.countMissing col))
   IColumnOps
-  (map-column [c f]
-    (case (-type-name (column-type c))
-      "DOUBLE"
-      "SINGLE"
-      "LONG"
-      "SHORT"
-      "INTEGER"
-      "FLOAT"
-      "BOOLEAN"
-      "STRING")
-    )
+  (map-column! [c f]
+    ;;todo -> look at coercing this to keys.
+    (do   (case (-type-name (column-type col)) 
+            "DOUBLE"  (typed-apply DoubleColumn  col f)  
+            "LONG"    (typed-apply LongColumn    col f)
+            "SHORT"   (typed-apply ShortColumn   col f)
+            "INTEGER" (typed-apply IntColumn     col f)
+            "FLOAT"   (typed-apply FloatColumn   col f)
+            "BOOLEAN" (typed-apply BooleanColumn col f) 
+            "STRING"  (typed-apply StringColumn  col f)
+            (throw (ex-info "unknown entry-type!" {:entry-type (-type-name (column-type c))
+                                                    :column-type (type col)})))
+          c))
   ;;convenience functions.
   clojure.lang.IFn
   (invoke [this row-index]
@@ -158,14 +159,13 @@
 
 ;;todo count-missing-by, missing-by?, missing, etc.
 
-(defn uniques [col]
+(defn unique [col]
   (if (extends? IUnique (type col))
     (-unique-set col)
     (-> col entries distinct)))
 
 (defn missing    [col])
 (defn missing-by [keyf col])
-
 
 (defn ^tech.tablesaw.io.csv.CsvReadOptions$Builder
   ->csv-builder [^String path & {:keys [separator header? date-format]}]
@@ -185,17 +185,23 @@
 (defn column->metadata
   [col]
   {:name (column-name col)
+   ;;revisit this...might have a better way of
+   ;;reporting common types, can eschew tablesaw.
    :type (-> col column-type type-name ->kebab-case)
    :size (entry-count col)
    :num-missing (count-missing col)
    :num-unique  (count-unique col)})
 
 
-;;deprecated
+;;deprecated, use protocol fn
 #_(defn column-name
   [^Column item]
   (.name item))
 
+;;this may be problematic / unnecessary since
+;;it breaks the tablesaw Table type.  Think more.
+;;We could implement column-map directly
+;;and provide associative access implementations.
 (defn col-seq->map
   [col-seq]
   (->> (columns col-seq)
@@ -208,54 +214,62 @@
     (->> (columns dataset)
          (map (comp #(get new-map %) column-name)))))
 
-;;given double-col -> getDouble
-;;given type -> (.set blah idx (double (fn  ... (.getType))))
-
-
+;;we may need to be able to define conversion, like as-double-column.
+;;TODO revisit this and think on it...
 (defn column-double-op
   [dataset column-name double-fn]
   (update-column
    dataset column-name
    (fn [^Column col]
      (let [^DoubleColumn double-col (.asDoubleColumn ^NumericColumn col)]
-       (parallel/parallel-for
+       (map-column! double-col double-fn double-col)
+       #_(parallel/parallel-for
         idx (entry-count double-col)
         (.set double-col idx
               (double (double-fn (.getDouble double-col idx)))))
-       double-col))))
+       #_double-col))))
 
+;;conenience function.
 (defn log1p
   [col-name dataset]
   (column-double-op dataset col-name #(Math/log (+ 1.0 (double %)))))
 
+;;TODO: looks at generalizing beyond relying on interop and NumericColumn...
+(defn numeric? [col]
+  (instance? NumericColumn col))
+
+;;TODO are these predicates or function of  :: dataset -> dataset?
 (defn numeric-missing?
-  [dataset]
-  (->> (columns dataset)
-       (filter #(and (instance? NumericColumn %)
-                     (> (.countMissing ^Column %) 0)))))
+  [col]
+  (and (numeric? col)
+       (pos? (count-missing col)))))
 
 (defn non-numeric-missing?
-  [dataset]
-  (->> (columns dataset)
-       (filter #(and (not (instance? NumericColumn %))
-                     (> (.countMissing ^Column %) 0)))))
+  [col]
+  (and (not (numeric? col))
+       (pos? (count-missing col))))
 
 (defn col-map
   [map-fn & args]
   (apply map map-fn (map columns args)))
 
+;;TODO: revisit this.  There's probably a common
+;;operation happening here, looks like we're trying to
+;;efficiently transform the backing string array, probably
+;;for labeling purposes...
 (defn update-strings
   [str-fn dataset]
-  (col-map (fn [^Column col]
+  (col-map (fn [col]
              (if (= "string" (:type (column->metadata col)))
                 (let [^"[Ljava.lang.String;" str-data (make-array String (.size col))]
                   (parallel/parallel-for
-                   idx (.size col)
+                   idx (entry-count col)
                    (aset str-data idx (str (str-fn (.getString ^StringColumn col idx)))))
-                  (StringColumn/create (.name col) str-data))
+                  (StringColumn/create (column-name col) str-data))
                 col))
            dataset))
 
+;;operations specific to coalesced dataset coercion?
 (def col-type->datatype-map
   {"short" :int16
    "integer" :int32})
@@ -263,19 +277,20 @@
 (defn col-datatype-cast
   [data-val ^Column column]
   (let [column-dtype-name (-> column
-                              (.type)
-                              (.name)
+                              column-type ;(.type)
+                              type-name  ;(.name)
                               ->kebab-case)]
     (if-let [dtype (get col-type->datatype-map column-dtype-name)]
       (dtype/cast data-val dtype)
       (throw (ex-info "Failed to map numeric datatype to datatype library"
                       {:column-type column-dtype-name})))))
 
+;;This is just filling in holes for missing data with a
+;;default value.
 (defn update-numeric-missing
   [num-val dataset]
   (col-map (fn [^Column col]
-             (if (and (instance? NumericColumn col)
-                      (> (.countMissing col) 0))
+             (if (numeric-missing? col)
                (let [new-col (.copy col)
                      ^ints missing-data (.toArray (.isMissing new-col))]
                  (parallel/parallel-for
@@ -291,19 +306,19 @@
 (defn get-column
   ^Column [column-map entry-kwd]
   (if-let [retval (get column-map entry-kwd)]
-    (:column retval)
+    (:column retval) ;;seems like it's wrong?
     (throw (ex-info (format "Failed to find column %s" entry-kwd)
                     {:columns (set (keys column-map))}))))
 
 ;;unknown what this is intended for...
 (defn set-string-table
   ^StringColumn [^StringColumn column str-table]
-  (when-not (= (column->unique-set column)
+  (when-not (= (unique column)
                (set (keys str-table)))
     (throw (ex-info "String table keys existing unique set mismatch"
                     {:str-table-keys (set (keys str-table))
-                     :column-unique-set (column->unique-set column)})))
-  (let [new-str-table (StringColumn/create (.name column))]
+                     :column-unique-set (unique column)})))
+  (let [new-str-table (StringColumn/create (column-name column))]
     ;;uggh.  Create the appropriate type for the size of the unique-set
     ;;and build the forward/reverse mappings.
     ;;Then set all the values and you should have it.
@@ -313,7 +328,7 @@
 
 
 ;;todo - replace with core.matrix?
-(defprotocol IColumn
+#_(defprotocol IColumn
   (get-entry [col row-idx]))
 
 (extend-protocol IColumn
@@ -334,10 +349,29 @@
           label-entry (get label-map col-kwd)]
       (if label-entry
         (fn [row-idx]
-          (let [retval (get label-entry (.get ^Column column (int row-idx)))]
+          (let [retval (get label-entry (get-entry column row-idx))]
             (checknan col-kwd row-idx retval)))
         #(get-entry )
 
+(fn [col-kwd]
+  (let [column (saw/get-column column-map col-kwd)
+        label-entry (get label-map col-kwd)]
+    (if label-entry
+      (fn [row-idx]
+        (let [retval (get label-entry (.get ^Column column (int row-idx)))]
+          (checknan col-kwd row-idx retval)))
+      (cond
+        (instance? NumericColumn column)
+        (fn [row-idx]
+          (checknan
+           col-kwd row-idx
+           (.getDouble ^NumericColumn column (int row-idx))))
+        (instance? BooleanColumn column)
+        (fn [row-idx]
+          (checknan
+           col-kwd row-idx
+           (.getDouble ^BooleanColumn column (int row-idx))))))))
+        
 
 (defn ->tech-ml-dataset
   [{:keys [label-map] :as options} dataset]
